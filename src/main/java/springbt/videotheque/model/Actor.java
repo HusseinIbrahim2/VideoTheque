93f3fe12a3be9a5da34defc81b08e7aae18855e5
Hussein Ibrahim <husseinib23920@gmail.com>
2025-10-05 19:36:41 +0200

    Add Actor model

--- src/main/java/springbt/videotheque/model/Actor.java
+++ src/main/java/springbt/videotheque/model/Actor.java
@@ -1,4 +1,36 @@
 package springbt.videotheque.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+
 public class Actor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "Name can't be empty")
+    private String name;
+
+    private String bio;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getBio() {
+        return bio;
+    }
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
 }
