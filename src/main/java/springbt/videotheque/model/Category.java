510e84758740364966fc936a3e6daca1d64fb2bc
Hussein Ibrahim <husseinib23920@gmail.com>
2025-10-05 19:33:37 +0200

    Add Category model

--- src/main/java/springbt/videotheque/model/Category.java
+++ src/main/java/springbt/videotheque/model/Category.java
@@ -1,4 +1,17 @@
 package springbt.videotheque.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
 public class Category {
+
+    @NotBlank(message = "Name can't be empty")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
