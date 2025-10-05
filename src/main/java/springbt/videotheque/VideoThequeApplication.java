a3571998960e2494f0b0c6b78353aa94b9ad6500
Hussein Ibrahim <husseinib23920@gmail.com>
2025-10-05 18:15:42 +0200

    Initial commit

---
+++ src/main/java/springbt/videotheque/VideoThequeApplication.java
@@ -0,0 +1,13 @@
+package springbt.videotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoThequeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoThequeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springbt/videotheque/VideoThequeApplicationTests.java
@@ -0,0 +1,13 @@
+package springbt.videotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoThequeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
