19c4328d5510491eca07bdfae19977f9764fe3b9
Hussein Ibrahim <husseinib23920@gmail.com>
2025-10-05 19:41:05 +0200

    Update category model

--- src/main/java/springbt/videotheque/model/Category.java
+++ src/main/java/springbt/videotheque/model/Category.java
@@ -1,13 +1,24 @@
 package springbt.videotheque.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
     @NotBlank(message = "Name can't be empty")
     private String name;
 
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
     public String getName() {
         return name;
     }
